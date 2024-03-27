8c764004badb0e470ad020c3e5c88d0e4ac151e0
Carlo Dandan <carlodandan.personal@proton.me>
2024-03-27 16:52:11 +0800

    gta9p: Import qcom packages for decryptions. * This preparation for decryption bringup later on.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- device.mk
+++ device.mk
@@ -14,3 +14,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+# qcom decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
