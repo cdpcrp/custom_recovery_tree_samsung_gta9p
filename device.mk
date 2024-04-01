11391c35b2b702598c96e3dc9f18f4cd6e6f4e02
Carlo Dee <carlodee.official@proton.me>
2024-04-01 13:46:25 +0000

    gta9p: Make copy of modules for depmod.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- device.mk
+++ device.mk
@@ -53,3 +53,6 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 32
+
+# Vendor Moduels | Copy modules for depmod
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/recovery/root/vendor/lib/modules/1.1,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
