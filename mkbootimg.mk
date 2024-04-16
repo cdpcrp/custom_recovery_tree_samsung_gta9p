a41ee5e8e59f525fb9eb555e08a1a1671cc8667e
Carlo Dee <carlodee.official@proton.me>
2024-04-16 15:39:00 +0800

    gta9p: Removed unnecessary command for bootimage.
    * We are building recoveryimage anyway.
    
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -5,13 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIMAGE_EXTRA_DEPS)
-	$(call pretty,"Target boot image: $@")
-	$(hide) $(MKBOOTIMG) --kernel $(call bootimage-to-kernel,$(1)) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
-	$(hide) echo -n "SEANDROIDENFORCE" >> $@
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
-	@echo "Made boot image: $@"
-
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel) $(RECOVERYIMAGE_EXTRA_DEPS) $(AVBTOOL)
 	@echo "----- Making recovery image ------"
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
