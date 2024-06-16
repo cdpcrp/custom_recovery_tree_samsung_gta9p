f925a54031a15e5ede6292c0cefccacb70f6ab4d
Carlo Dee <carlodee.official@proton.me>
2024-06-17 06:26:57 +0800

    gta9p: Switched `fastboot` back to version 1.0
    * It should have been 1.0 anyway, it matches stock and also, currently, TWRP only support this version better than 1.1
    
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock \
     fastbootd   
 
 # Recovery modules/dependencies
